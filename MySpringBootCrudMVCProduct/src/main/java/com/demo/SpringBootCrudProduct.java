6db0abc62b8815745b10320e48a36c2a285a35aa
prathamesh nale <prathameshn7077@gmail.com>
2024-01-19 21:39:10 +0530

    SpringBootCrudProduct

---
+++ MySpringBootCrudMVCProduct/src/main/java/com/demo/SpringBootCrudProduct.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudProduct {
+	public static void main(String[] args) {
+		//it returns ApplicationContext
+		SpringApplication.run(SpringBootCrudProduct.class,args);
+	}
+
+}
