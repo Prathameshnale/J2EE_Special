3e01aaf362eb67c1a3694a5dc58fd5c76a39931c
prathamesh nale <prathameshn7077@gmail.com>
2024-02-01 22:46:32 +0530

    added

---
+++ Modul_End/main/java/com/demo/XYZ_Company_MVC.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class XYZ_Company_MVC {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(XYZ_Company_MVC.class,args);
+		
+	}
+
+}
---
+++ Modul_End/main/java/com/demo/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.demo.service;
+
+import java.util.List;
+
+import com.demo.model.Employee;
+
+public interface EmployeeService {
+
+	
+
+	void addLeave(Employee employee);
+
+	List<Employee> getAllEmployee();
+
+}
