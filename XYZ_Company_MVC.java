d6ab013f92e2374461b715a5129976d4fad6a88c
Prathamesh Nale <141139818+Prathameshnale@users.noreply.github.com>
2024-02-17 15:32:58 +0530

    Update XYZ_Company_MVC.java

--- XYZ_Company_MVC.java
+++ XYZ_Company_MVC.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class XYZ_Company_MVC {
 
+	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(XYZ_Company_MVC.class,args);
